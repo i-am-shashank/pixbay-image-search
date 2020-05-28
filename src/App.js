34bb80ae0d6e3c2b0c837c6daac6d96a2cfca017
Shankymous <60059208+Shankymous@users.noreply.github.com>
2020-05-28 06:56:54 -0700

    image finder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/navbar/NavBar';
+import Search from './components/search/Search';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
